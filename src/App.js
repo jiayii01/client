9b77a44754b11a0435da64dceefe950c69db8765
jiayii01 <79521323+jiayii01@users.noreply.github.com>
2023-06-04 22:35:33 +0800

    edited the input groups

--- src/App.js
+++ src/App.js
@@ -33,74 +33,60 @@ const App = () => {
 
   return (
     <div className='app'>
-      <nav>
-        <ul className='nav-bar'>
-          <li>Attractions</li>
-        </ul>
+      <nav class="navbar navbar-light bg-light">
+        <span class="navbar-brand m-2 h1"><b>Singapore Attractions</b></span>
       </nav>
       <p className='directions'>
         {' '}
-        Search for earthquakes using the following criteria:
+        Search for attractions in Singapore using the following criteria:
       </p>
       <div className='main'>
         <div className='type-selector'>
+        <div class="row g-2">
+          <div class="col-md">
+            <form>
+              <input
+                className='form'
+                type='text' class="form-control" id="floatingInputGrid"
+                placeholder='Search for anything!'
+                value={chosenSearch}
+                onChange={(e) => setChosenSearch(e.target.value)}
+              />
+              </form>
+          </div>
+          <div className="col-md">
+            <select
+              name='types'
+              id='types' class="form-select"
+              value={chosenType}
+              onChange={(e) => setChosenType(e.target.value)}
+            >
+              <option value={null}>Select a Type</option>
+              <option value='Nature & Wildlife'>Nature & Wildlife</option>
+              <option value='Leisure & Recreation'>Leisure & Recreation</option>
+              <option value='Adventure'>Adventure</option>
+              <option value='History & Culture'>History & Culture</option>
+              <option value='Arts'>Arts</option>
+              <option value='Others'>Others</option>
+            </select>
+          </div>
+          <div className="col-md">
+            <select
+              name='ratingRange'
+              id='ratingRange' class="form-select"
+              value={chosenRatingRange}
+              onChange={(e) => setChosenRatingRange(e.target.value)}
+            >
+              <option value={null}>Ratings Above</option>
+              <option value='2'>2</option>
+              <option value='3'>3</option>
+              <option value='4'>4</option>
+              <option value='5'>5</option>
+            </select>
+          </div>
+        </div>
           <ul>
-            <li>
-              <form>
-                <label>
-                  <input
-                    className='form'
-                    type='text'
-                    placeholder='Search for anything!'
-                    value={chosenSearch}
-                    onChange={(e) => setChosenSearch(e.target.value)}
-                  />
-                </label>
-              </form>
-            </li>
-            <li>
-              <select
-                name='types'
-                id='types'
-                value={chosenType}
-                onChange={(e) => setChosenType(e.target.value)}
-              >
-                <option value={null}>Select a Type</option>
-                <option value='Nature & Wildlife'>Nature & Wildlife</option>
-                <option value='Leisure & Recreation'>Leisure & Recreation</option>
-                <option value='Adventure'>Adventure</option>
-                <option value='History & Culture'>History & Culture</option>
-                <option value='Arts'>Arts</option>
-                <option value='Others'>Others</option>
-              </select>
-            </li>
-            <li>
-              <select
-                name='ratingRange'
-                id='ratingRange'
-                value={chosenRatingRange}
-                onChange={(e) => setChosenRatingRange(e.target.value)}
-              >
-                <option value={null}>Ratings Above</option>
-                <option value='2'>2</option>
-                <option value='3'>3</option>
-                <option value='4'>4</option>
-                <option value='5'>5</option>
-              </select>
-            </li>
-            {/* <li>
-              <form>
-                <label>
-                  <input
-                    className='form'
-                    type='text'
-                    placeholder='Enter city, state, country'
-                    value={chosenLocation}
-                    onChange={(e) => setChosenLocation(e.target.value)}
-                  />
-                </label>
-              </form>
-            </li> */}
+          
             <li>
               <select
                 name='sortOption'
@@ -128,23 +114,21 @@ const App = () => {
             <div className="row">
                 {documents.map((document) => (
                   <div className="col-md-4">
-                  <div class="card m-1" style={{width: '25rem', height: '15rem'}}>
+                  <div class="card m-1" style={{width: '25rem', height: '19rem'}}>
                     <div class="card-body">
                       <h5 class="card-title">{document._source.name}<span class="badge rounded-pill text-bg-success ms-4">{document._source.rating}</span></h5>
                       <span class="badge text-bg-primary">{document._source.type}</span>
-                      <p class="card-text">Tags: {document._source.tags}</p>
-                      <p>Address: {document._source.block} {document._source.streetName} {document._source.floorNumber} {document._source.unitNumber} {document._source.buildingName} {document._source.postalCode}</p>
-                      <p>Official Site: {document._source.officialWebsite}</p>
+                      <p class="m-1">Address: {document._source.block} {document._source.streetName} {document._source.floorNumber} {document._source.unitNumber} {document._source.buildingName} {document._source.postalCode}</p>
+                      {
+                        document._source.tags.map((tag) => (
+                          <span class="badge text-bg-light">{tag}</span>
+                        ))
+                      }
+                        {/* <small>Tags: {document._source.tags}</small> */}
+                      <p><small>Official Site: {document._source.officialWebsite}</small></p>
                     </div>
                   </div>
                   </div>
-                //   <div className='results-card'>
-                //     <div className='results-text'>
-                //       <p>Rating: {document._source.rating}</p>
-                //       <p>Address: {document._source.block} {document._source.streetName} {document._source.floorNumber} {document._source.unitNumber} {document._source.buildingName} {document._source.postalCode}</p>
-                //       <p>Official Site: {document._source.officialWebsite}</p>
-                //     </div>
-                //   </div>
                 ))}
               
             </div>
